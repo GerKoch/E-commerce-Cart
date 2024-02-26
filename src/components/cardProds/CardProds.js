3df522ca94ea7f33d3be024b5b74450dad47a6e0
German Koch <gjkoch7@gmail.com>
2024-02-26 20:40:28 -0300

    testChangesCart

--- src/components/cardProds/CardProds.js
+++ src/components/cardProds/CardProds.js
@@ -49,7 +49,7 @@ export const CardProds = ({ data }) => {
             {quantityPerItem > 0 && (
                 <div>{quantityPerItem}</div>
             )}
-            <p>{data.title}</p>
+            <p>lala{data.title}</p>
             <img alt={data.title} src={data.image} />
             <p>${data.price}</p>
             {quantityPerItem === 0 ? (
