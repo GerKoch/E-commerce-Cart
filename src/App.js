5537fa0a8d765a7c32cd48c4a0307a17aef0b47b
JeremiasKoch <jeremiaskoch.92@gmail.com>
2024-03-07 18:42:16 -0300

    fix: deleted unused file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Products } from "../src/pages/products/Products";
+import { Navbar } from "../src/components/navbar/Navbar";
+import { ShoppingCart } from "../src/components/shoppingCart/ShoppingCart";
+import { ShoppingCartProvider } from "../src/components/contexts/ShoppingCartContext";
 
 function App() {
   return (
     <div>
-      <Dashboard />
+      <ShoppingCartProvider>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Products />} />
+                <Route path="/cart" element={<ShoppingCart />} />
+            </Routes>
+        </Router>
+      </ShoppingCartProvider>
     </div>
   )
 }
