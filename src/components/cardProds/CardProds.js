d6d783b48566fe3c7a927bcfb2212c3f23944300
German Koch <gjkoch7@gmail.com>
2024-03-25 21:43:31 -0300

    Se mejora CardProds

--- src/components/cardProds/CardProds.js
+++ src/components/cardProds/CardProds.js
@@ -2,14 +2,15 @@ import "./CardProds.css";
 import "boxicons";
 import { AddSubtract } from "../../utils/addSubtract/AddSubtract";
 
- export const CardProds = ({ data }) => {
+export const CardProds = ({ data }) => {
 
     return (
         <div>
-            <div className="container">furtherutils
+            <div className="container">
+                <figure className="containerImg">
+                    <img alt={data.title} src={data.image} />
+                </figure>
                 <p>{data.title}</p>
-                <img alt={data.title} src={data.image} />
-                <box-icon name='heart' className="cardHeart"></box-icon>
                 <div className="bubble">
                     <p>${data.price}</p>
                 </div>
